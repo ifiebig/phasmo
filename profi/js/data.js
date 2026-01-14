36bae0b4e59fa8bd6fd782343bc19b0c3df78846
phasmo <i.fiebig@gmx.net>
2026-01-14 22:56:23 +0100

    Add files via upload

---
+++ profi/js/data.js
@@ -0,0 +1,14 @@
+const evidences = [
+  "EMF 5",
+  "Gefriertemperatur",
+  "Geisterbuch",
+  "Fingerabdrücke",
+  "Orbs",
+  "DOTS",
+  "Spirit Box"
+];
+
+const ghosts = [
+  { name: "Banshee", evidence: ["DOTS", "Orbs", "Fingerabdrücke"] },
+  { name: "Obambo", evidence: ["EMF 5", "Geisterbuch", "DOTS"] }
+];
